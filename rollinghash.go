af7ddd6b29c327380d00dfdf1fec822875b49c49
korci <korcanucar@gmail.com>
2020-04-02 22:30:03 +0200

    fixed unsigned to signed int

--- rollinghash.go
+++ rollinghash.go
@@ -6,49 +6,51 @@ import (
 )
 
 var myhash int = 0
-var hashtooffset map[uint64]int
-var MODULAR uint64 = 1000000007
-var BASE uint64 = 253
-
-func generatePower(p uint64, stringsize int) uint64 {
-	var power uint64 = 1
-	var base uint64 = 257
-	var overflow_control uint64 = 1000000007
-	for i := 0; i < stringsize; i++ {
-		power = (power * base) % overflow_control
-	}
-	return power
-}
+var hashtooffset map[int64]int
+var MODULAR int64 = 928223
+var BASE int64 = 10
 
 // find first common string of length L  in two strings A, B
 
-func pow(base uint64, power int) uint64 {
-	return (uint64)(math.Pow((float64)(base), (float64)(power)))
+func pow(base int64, power int, modular int64) int64 {
+	return (int64)(math.Pow((float64)(base), (float64)(power))) % modular
 }
 
-func computeHash(mystring string, base uint64, modular uint64) uint64 {
-	var sum uint64 = 0
+func computeHash(mystring string, base int64, modular int64) int64 {
+	var ret int64 = 0
 	for i := 0; i < len(mystring); i++ {
-		sum += (uint64)(mystring[i]) * pow(base, (len(mystring)-1-i))
+		ret = ret*base + (int64)(mystring[i])
+		ret %= modular
 	}
-	return sum % modular
+	return ret
 }
 
-func computeRoll(mystring string, rollength int, prevhash uint64, base uint64, firstchar byte, modular uint64) uint64 {
-	var hsh uint64 = 0
-	if prevhash == 0 {
-		prevhash = computeHash(mystring, base, MODULAR)
+func computePower(base int64, power int, modular int64) int64 {
+	var ret int64 = 1
+	for i := 0; i < power; i++ {
+		ret = (ret * base) % modular
 	}
+	return ret
+}
+func computeRoll(mystring string, rollength int, prevhash int64, base int64, power int64, firstchar byte, modular int64) int64 {
+	var hsh int64 = 0
+
 	lastchar := mystring[len(mystring)-1]
-	hsh = base*(prevhash-((uint64)(firstchar)*pow(base, rollength-1))) + (uint64)(lastchar)
+
+	hsh = base*(prevhash-((int64)(firstchar)*power)%modular) + (int64)(lastchar)
+
+	for hsh < 0 {
+		hsh = hsh + modular
+	}
+	// fmt.Println(base, "*(", prevhash, "-((", firstchar, ")*", power, "))+", lastchar)
 	return hsh % modular
 }
 
 //iterate one char and calculate hash of each 3 char chunks, add to map
-func createHashmap(mystring string, substrsize uint64) {
+func createHashmap(mystring string, substrsize int64) {
 	fmt.Println("createHash for ", mystring)
 	if nil == hashtooffset {
-		hashtooffset = make(map[uint64]int)
+		hashtooffset = make(map[int64]int)
 	}
 
 	for i := 0; i < len(mystring)-(int)(substrsize-1); i++ {
@@ -59,25 +61,25 @@ func createHashmap(mystring string, substrsize uint64) {
 
 }
 
-func Rollinghash(findthis string, inthis string, k int) map[string][]int {
+func Rollinghash(findthis string, inthis string, windowlen int) map[string][]int {
 
 	str2indexes := make(map[string][]int)
-
-	fmt.Println("rollinghash: trying to find pattern ", findthis, " in ", inthis, " k = ", k)
-	if len(inthis) <= k {
+	p := computePower(BASE, windowlen-1, MODULAR)
+	fmt.Println("rollinghash: trying to find pattern ", findthis, " in ", inthis, " windowlen = ", windowlen)
+	if len(inthis) <= windowlen {
 		panic("wrong length of string ")
 	}
-	createHashmap(findthis, (uint64)(k))
-	var hsh uint64 = computeHash(inthis[0:k], BASE, MODULAR)
+	createHashmap(findthis, (int64)(windowlen))
+	var hsh int64 = computeHash(inthis[0:windowlen], BASE, MODULAR)
 	firstcharinwindow := inthis[0]
-	fmt.Println("computed roll hash for ", inthis[0:k], " = ", hsh, "firstchar=", firstcharinwindow)
-	for i := 1; i < len(inthis)-k+1; i++ {
-		hsh = computeRoll(inthis[i:i+k], k, hsh, BASE, firstcharinwindow, MODULAR)
-		fmt.Println("computed roll hash for ", inthis[i:i+k], " = ", hsh, "firstchar=", firstcharinwindow)
+	fmt.Println("computed roll hash for ", inthis[0:windowlen], " = ", hsh, "firstchar=", firstcharinwindow)
+	for i := 1; i < len(inthis)-windowlen+1; i++ {
+		hsh = computeRoll(inthis[i:i+windowlen], windowlen, hsh, BASE, p, firstcharinwindow, MODULAR)
+		fmt.Println("computed roll hash for ", inthis[i:i+windowlen], " = ", hsh, "firstchar=", firstcharinwindow)
 		firstcharinwindow = inthis[i]
 		if res, ok := hashtooffset[hsh]; ok {
-			str2indexes[findthis[res:res+k]] = append(str2indexes[findthis[res:res+k]], i)
-			fmt.Println("found  occurrance at ", i, " for pattern ", findthis[res:res+k])
+			str2indexes[findthis[res:res+windowlen]] = append(str2indexes[findthis[res:res+windowlen]], i)
+			fmt.Println("found  occurrance at ", i, " for pattern ", findthis[res:res+windowlen])
 		}
 	}
 	return str2indexes
