1d48b3a35ffe32a5e8ac5e1a622559237eac415b
korci <korcanucar@gmail.com>
2020-04-06 21:34:15 +0200

    minor fix

--- rollinghash.go
+++ rollinghash.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 var myhash int = 0
 var hashtooffset map[int64]int
-var MODULAR int64 = 928223
+var MODULAR int64 = 928223 //some prime number
 var BASE int64 = 10
 
-// find first common string of length L  in two strings A, B
-
-func pow(base int64, power int, modular int64) int64 {
-	return (int64)(math.Pow((float64)(base), (float64)(power))) % modular
-}
-
+//computeHash does the hashing according to the following :
+// hash("abc")= ((a)*base^2 + (b)*base^1 + (c)*base^0) % (modular)
+//
 func computeHash(mystring string, base int64, modular int64) int64 {
 	var ret int64 = 0
 	for i := 0; i < len(mystring); i++ {
@@ -25,6 +21,8 @@ func computeHash(mystring string, base int64, modular int64) int64 {
 	return ret
 }
 
+//computePower does nothing except calculating
+//(base^power) % (modular)
 func computePower(base int64, power int, modular int64) int64 {
 	var ret int64 = 1
 	for i := 0; i < power; i++ {
@@ -32,13 +30,15 @@ func computePower(base int64, power int, modular int64) int64 {
 	}
 	return ret
 }
+
+//computeRoll does the rolling hash computation for "bcd" string
+//given the hash of "abc". It does not calculate hash("bcd") from scratch
+//just calculate it with the given previous hash calculation.
 func computeRoll(mystring string, rollength int, prevhash int64, base int64, power int64, firstchar byte, modular int64) int64 {
 	var hsh int64 = 0
 
 	lastchar := mystring[len(mystring)-1]
-
 	hsh = base*(prevhash-((int64)(firstchar)*power)%modular) + (int64)(lastchar)
-
 	for hsh < 0 {
 		hsh = hsh + modular
 	}
@@ -46,36 +46,37 @@ func computeRoll(mystring string, rollength int, prevhash int64, base int64, pow
 	return hsh % modular
 }
 
-//iterate one char and calculate hash of each 3 char chunks, add to map
+//iterate one char and calculate hash of each substrsize char chunks, add to map
 func createHashmap(mystring string, substrsize int64) {
-	fmt.Println("createHash for ", mystring)
 	if nil == hashtooffset {
 		hashtooffset = make(map[int64]int)
 	}
-
 	for i := 0; i < len(mystring)-(int)(substrsize-1); i++ {
 		hsh := computeHash(mystring[i:i+(int)(substrsize)], BASE, MODULAR)
 		hashtooffset[hsh] = i
-		fmt.Println(mystring[i:i+(int)(substrsize)], "---", hsh, "----", i)
+		// fmt.Println(mystring[i:i+(int)(substrsize)], "---", hsh, "----", i)
 	}
 
 }
 
+//Rollinghash is the application Rabin-Karp Algorithm to find  chunks of
+//length N of the "pattern" string in the big string to search.
+//it returns a map of pattern -> index in the searched string
 func Rollinghash(findthis string, inthis string, windowlen int) map[string][]int {
 
 	str2indexes := make(map[string][]int)
 	p := computePower(BASE, windowlen-1, MODULAR)
-	fmt.Println("rollinghash: trying to find pattern ", findthis, " in ", inthis, " windowlen = ", windowlen)
+	// fmt.Println("rollinghash: trying to find pattern ", findthis, " in ", inthis, " windowlen = ", windowlen)
 	if len(inthis) <= windowlen {
 		panic("wrong length of string ")
 	}
 	createHashmap(findthis, (int64)(windowlen))
 	var hsh int64 = computeHash(inthis[0:windowlen], BASE, MODULAR)
 	firstcharinwindow := inthis[0]
-	fmt.Println("computed roll hash for ", inthis[0:windowlen], " = ", hsh, "firstchar=", firstcharinwindow)
+	// fmt.Println("computed roll hash for ", inthis[0:windowlen], " = ", hsh, "firstchar=", firstcharinwindow)
 	for i := 1; i < len(inthis)-windowlen+1; i++ {
 		hsh = computeRoll(inthis[i:i+windowlen], windowlen, hsh, BASE, p, firstcharinwindow, MODULAR)
-		fmt.Println("computed roll hash for ", inthis[i:i+windowlen], " = ", hsh, "firstchar=", firstcharinwindow)
+		// fmt.Println("computed roll hash for ", inthis[i:i+windowlen], " = ", hsh, "firstchar=", firstcharinwindow)
 		firstcharinwindow = inthis[i]
 		if res, ok := hashtooffset[hsh]; ok {
 			str2indexes[findthis[res:res+windowlen]] = append(str2indexes[findthis[res:res+windowlen]], i)
--- rollinghash_test.go
+++ rollinghash_test.go
@@ -1,31 +1,64 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"testing"
 )
 
 func TestResultLenVal(t *testing.T) {
-	str1 := "abcdefghijklmnoprstuvyz"
-	str2 := "rvyz"
+	haystack := "abcdefghijklmnoprstuvyz"
+	needle := "rvyz"
 
-	ret := Rollinghash(str2, str1, 3)
-
-	t.Errorf("len of result slice is incorrect actual :%d , expected:%d", len(ret), 1)
+	ret := Rollinghash(needle, haystack, 3)
+	//test the index of occurance
+	if ret["vyz"][0] != 20 {
+		t.Errorf("index of substring is incorrect actual  :%d , expected:%d", ret[needle], 7)
+	}
 
 }
 
-func TestFastAfile(t *testing.T) {
-	// content, err := ioutil.ReadFile("fasta")
-	// Convert []byte to string and print to screen
-	// str1 := string(content)
-	str2 := "KORCANCCAACCA"
-	str3 := "CTTCCCAKORCANCCAACCAACTENDTTCGATCTCTTGTAGATCTGTTCTCTAAACGAACTTTAAAATCT"
+func TestFindLongSubstring(t *testing.T) {
 
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	needle := "KORCANCCAACCAACTEND"
+	haystack := "CTTCCCAKORCANCCAACCAACTENDTTCGATCTCTTGTAGATCTGTTCTCTAAACGAACTTTAAAATCT"
+	ret := Rollinghash(needle, haystack, len(needle))
+	//test number of results
+	if len(ret) != 1 {
+		t.Errorf("len of result map is incorrect actual :%d , expected:%d", len(ret), 1)
+	}
+	//test number of occurrance
+	if len(ret[needle]) != 1 {
+		t.Errorf("index of substring is incorrect actual  :%d , expected:%d", ret[needle], 1)
+	}
+	//test the index of occurance
+	if ret[needle][0] != 7 {
+		t.Errorf("index of substring is incorrect actual  :%d , expected:%d", ret[needle], 7)
+	}
 
-	ret := Rollinghash(str2, str3, len(str2))
-	t.Errorf("len of result slice is incorrect actual :%d , expected:%d", len(ret), 1)
+}
+func TestFindLineInFastAFile(t *testing.T) {
+	content, err := ioutil.ReadFile("sars_cov2_fasta.txt")
+	//Convert []byte to string and print to screen
+	haystack := string(content)
+	needle := "CCTTCAAAATTATGGTGATAGTGCAACATTACCTAAAGGCATAATGATGAATGTCGCAAAATATACTCAA"
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ret := Rollinghash(needle, haystack, len(needle))
+	//test number of results
+	if len(ret) != 1 {
+		t.Errorf("len of result map is incorrect actual :%d , expected:%d", len(ret), 1)
+	}
+	//test number of occurrance
+	if len(ret[needle]) != 1 {
+		t.Errorf("index of substring is incorrect actual  :%d , expected:%d", ret[needle], 1)
+	}
+	//test the index of occurance
+	if ret[needle][0] != 20946 {
+		t.Errorf("index of substring is incorrect actual  :%d , expected:%d", ret[needle], 7)
+	}
 
 }
